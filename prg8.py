73ed55398c2c110c83b7b9873bbedff3a854616e
tessa davis <67496079+tessadt@users.noreply.github.com>
2023-11-09 20:24:01 -0800

    Add files via upload

---
+++ prg8.py
@@ -0,0 +1,15 @@
+n=int(input("enter the number of prime to find:"))
+count=0
+a=2
+while True:
+    flag=0
+    for i in range(2,a//2+1):
+        if a%i==0:
+            flag=1
+            break
+    if flag==0:
+        print(a)
+        count=count+1
+    if count==n:
+        break
+    a=a+1
