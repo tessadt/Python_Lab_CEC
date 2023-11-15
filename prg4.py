c26cc35dd8fe79d7ab8a55869500f91f1e714eb8
tessa davis <67496079+tessadt@users.noreply.github.com>
2023-11-15 15:51:45 +0530

    Add files via upload

---
+++ prg4.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if a>b and a>c:
+    print("The largest number is  :",a)
+elif  b>a and b>c:
+    print("the largest number is :",b)
+else:
+   print("the largest number is :",c)
+
